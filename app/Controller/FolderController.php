b0cb54a5b550a1b8a317b1c2df8438f5b32c4a70
Don Drake <ddrake@dreamingmind.com>
2015-01-09 13:55:39 -0800

    Add a folder controller

---
+++ app/Controller/FolderController.php
@@ -0,0 +1,27 @@
+<?php
+App::uses('BranchNode', 'Lib');
+/**
+ * Description of FolderController
+ *
+ * @author dondrake
+ */
+class FolderController extends BranchNode {
+	
+	
+	public function index() {
+		
+	}
+
+	public function name() {
+		
+	}
+
+	public function search($value) {
+		
+	}
+
+	public function summary() {
+		
+	}
+
+}
